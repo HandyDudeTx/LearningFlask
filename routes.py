ea992a3baa0e13c2a5b8c7dcfbaabb7d3484443f
Jeffrey Hutchens <jah8313@gmail.com>
2018-12-11 17:57:08 -0600

    Reset Example Files

--- routes.py
+++ routes.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 from models import db, User
 from forms import SignupForm
 
-
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/learningflask'
@@ -12,26 +11,27 @@ app.secret_key = "development-key"
 
 @app.route("/")
 def index():
-	return render_template("index.html")
+  return render_template("index.html")
 
 @app.route("/about")
 def about():
-	return render_template("about.html")
+  return render_template("about.html")
 
-@app.route("/signup", methods=['GET', 'POST'])
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
-	form = SignupForm()
-	if request.method == 'POST':
-		if form.validate() == False:
-			return render_template('signup.html', form=form)
-		else:
-			newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
-			db.session.add(newuser)
-			db.session.commit()
-			return "Success!"
+  form = SignupForm()
 
-	elif request.method == 'GET':
-		return render_template('signup.html', form=form)
+  if request.method == "POST":
+    if form.validate() == False:
+      return render_template('signup.html', form=form)
+    else:
+      newuser = User(form.first_name.data, form.last_name.data, form.email.data, form.password.data)
+      db.session.add(newuser)
+      db.session.commit()
+      return 'Success!'
+
+  elif request.method == "GET":
+    return render_template('signup.html', form=form)
 
 if __name__ == "__main__":
-	app.run(debug=True)
+  app.run(debug=True)
